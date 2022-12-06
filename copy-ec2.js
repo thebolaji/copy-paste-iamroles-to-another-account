e0f619bb9c2e0e55d22c894e551a5cd9d3c4b650
bigb97 <bigbofficial01@gmail.com>
2022-12-06 19:54:39 +0100

    ec2 is ready

--- copy-ec2.js
+++ copy-ec2.js
@@ -1,4 +1,5 @@
 const AWS = require("aws-sdk");
+const { error } = require("console");
 const fs = require("fs");
 
 async function loadAWSCredentials(profileName) {
@@ -12,23 +13,18 @@ async function loadAWSCredentials(profileName) {
   log("<-- AWS credentials loaded.");
 }
 
-// Name
-// OS name
-// OS Version
-// IP address
-// AWS Account
-// Description
-
 // List all EC2 instances from all regions
 (async () => {
   await loadAWSCredentials("production");
-  const ec2 = new AWS.EC2({
-    region: "eu-west-2",
-  });
-  const listRegions = await ec2.describeRegions().promise();
-  let regions = listRegions.Regions.map((region) => region.RegionName);
-  for (let i = 0; i < regions.length; i++) {
-    const getInstancesinFromEachRegion = await ec2
+  let ec2 = new AWS.EC2({ apiVersion: "2016-11-15", region: "eu-west-2" });
+  const regions = await ec2.describeRegions().promise();
+
+  regions.Regions.forEach(async (region) => {
+    let newEC2 = new AWS.EC2({
+      apiVersion: "2016-11-15",
+      region: region.RegionName,
+    });
+    const foundInstances = await newEC2
       .describeInstances({
         Filters: [
           {
@@ -38,20 +34,67 @@ async function loadAWSCredentials(profileName) {
         ],
       })
       .promise();
-    getInstancesinFromEachRegion.Reservations.forEach((reservation) => {
-      reservation.Instances.forEach((instance) => {
+    if (foundInstances.Reservations.length > 0) {
+      for (let i = 0; i < foundInstances.Reservations.length; i++) {
+        const instance = foundInstances.Reservations[i].Instances[0];
+        // apend to file and add comma at the end
+
         fs.appendFile(
           "ec2-instances.json",
-          JSON.stringify(instance, null, 2),
+          JSON.stringify(
+            {
+              Name: instance.Tags[0].Value,
+              OS: instance.ImageId,
+              PrivateIpAddress: instance.PrivateIpAddress,
+              PublicIpAddress: instance.PublicIpAddress,
+              InstanceId: instance.InstanceId,
+              AwsRegion: region.RegionName,
+              AwsAccount: AWS.config.credentials.accessKeyId,
+            },
+            null,
+            2
+          ) + ",",
           (err) => {
             if (err) {
               console.log(err);
             }
           }
         );
-      });
-    });
-  }
+      }
+    }
+  });
+  //   const getInstances = await ec2.describeInstances().promise();
+  // get all ec2 instances
+
+  //   console.log(getInstances);
+
+  //   get all regions
+  //   check if an ec2 instance is running in a region
+  //   regions.Regions.forEach(async (region) => {
+  //     const getInstancesinRegion = await ec2
+  //       .describeInstances({
+  //         Filters: [
+  //           {
+  //             Name: "instance-state-name",
+  //             Values: ["running"],
+  //           },
+  //         ],
+  //       })
+  //       .promise();
+  //     getInstancesinRegion.Reservations.forEach((reservation) => {
+  //       reservation.Instances.forEach((instance) => {
+  //         fs.writeFile(
+  //           "ec2-instances.json",
+  //           JSON.stringify(instance, null, 2),
+  //           (err) => {
+  //             if (err) {
+  //               console.log(err);
+  //             }
+  //           }
+  //         );
+  //       });
+  //     });
+  //   });
 })();
 
 function log(message) {
